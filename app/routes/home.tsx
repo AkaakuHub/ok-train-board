c7b053186da9df4c7d3fb5fedf3aacf451994eeb
AkaakuHub <Akaaku2021media01@gmail.com>
2025-04-25 02:45:11 +0900

    fix: OGP URL

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -2,17 +2,19 @@ import { Welcome } from "~/welcome/welcome";
 import type { Route } from "./+types/home";
 
 export function meta({}: Route.MetaArgs) {
+  const VITE_BASE_URL = import.meta.env.VITE_BASE_URL ?? "";
   return [
     { title: "OK-TRAIN 電光掲示板" },
     { name: "description", content: "Welcome to the 電光掲示板!" },
     { name: "twitter:card", content: "summary" },
     { name: "twitter:title", content: "OK-TRAIN 電光掲示板" },
     { name: "twitter:description", content: "Welcome to the 電光掲示板!" },
-    { name: "twitter:image", content: "/images/ogp.webp" },
+    { name: "twitter:image", content: `${VITE_BASE_URL}/images/ogp.webp` },
     { name: "og:title", content: "OK-TRAIN 電光掲示板" },
     { name: "og:description", content: "Welcome to the 電光掲示板!" },
-    { name: "og:image", content: "/images/ogp.webp" },
-    { name: "og:url", content: "/" },
+    { name: "og:image", content: `${VITE_BASE_URL}/images/ogp.webp` },
+    { name: "og:url", content: `${VITE_BASE_URL}/` },
+    { name: "og:locale", content: "ja_JP" },
     { name: "og:type", content: "website" },
     { name: "og:site_name", content: "OK-TRAIN 電光掲示板" },
   ];
